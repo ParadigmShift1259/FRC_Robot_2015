0f2da6f27fca35f6fa7883ea7b03c0126a2ee2f4
scubadiv3r <allisoncconrad@gmail.com>
2015-01-22 19:21:52 -0600

    Awesome code of amazingness

--- Emlar2015/src/OI.cpp
+++ Emlar2015/src/OI.cpp
@@ -14,7 +14,7 @@ OI::OI(Joystick* mainJoystick2) {
 	mainJoystick = mainJoystick2;
 }
 double OI::GetX() {
-	double currentX = mainJoystick->GetX();
+	double currentX = (mainJoystick->GetX()/OI::GetThrottle());
 	//adds deadzone and ramping
 	if (currentX > deadzone || currentX < -deadzone) {
 		if (std::abs(currentX - x) < shiftPerCycle) {
@@ -25,12 +25,19 @@ double OI::GetX() {
 			x = x - shiftPerCycle;
 		}
 	} else {
-		x = 0;
+		currentX = 0;
+		if (std::abs(currentX - x) < shiftPerCycle) {
+			x = currentX;
+		} else if (currentX > x) {
+			x = x + shiftPerCycle;
+		} else {
+			x = x - shiftPerCycle;
+		}
 	}
 	return x;
 }
 double OI::GetY() {
-	double currentY = mainJoystick->GetY();
+	double currentY = (mainJoystick->GetY()/OI::GetThrottle());
 	//adds deadzone and ramping
 	if (currentY > deadzone || currentY < -deadzone) {
 		if (std::abs(currentY - y) < shiftPerCycle) {
@@ -41,35 +48,41 @@ double OI::GetY() {
 			y = y - shiftPerCycle;
 		}
 	} else {
-		y = 0;
+		currentY = 0;
+		if (std::abs(currentY - y) < shiftPerCycle) {
+			y = currentY;
+		} else if (currentY > y) {
+			y = y + shiftPerCycle;
+		} else {
+			y = y - shiftPerCycle;
+		}
 	}
 	return y;
 }
+
 double OI::GetTwist() {
-	double currentTwist = mainJoystick->GetTwist();
-	//adds deadzone and ramping
-	if (currentTwist > deadzone || currentTwist < -deadzone) {
-		if (std::abs(currentTwist - twist) < shiftPerCycle) {
-			twist = currentTwist;
-		} else if (currentTwist > twist) {
-			twist = twist + shiftPerCycle;
-		} else {
-			twist = twist - shiftPerCycle;
-		}
+	double currentTwist = (mainJoystick->GetTwist()/OI::GetThrottle());
+	//adds deadzone
+	if (currentTwist > twistDeadzone || currentTwist < -twistDeadzone) {
+		twist = currentTwist;
 	} else {
 		twist = 0;
 	}
 	return twist;
 }
 
-bool OI::GetTrigger(){
-	if(!lastTriggered&&mainJoystick->GetTrigger())
-	{
-		triggerState=!triggerState;
+bool OI::GetTrigger() {
+	if (!lastTriggered && mainJoystick->GetTrigger()) {
+		triggerState = !triggerState;
 	}
 	lastTriggered = mainJoystick->GetTrigger();
 	return triggerState;
 }
+
+double OI::GetThrottle() {
+	return mainJoystick->GetThrottle()+2.1;
+}
+
 Joystick* OI::GetJoystick() const {
 	return mainJoystick;
 }
--- Emlar2015/src/Robot.cpp
+++ Emlar2015/src/Robot.cpp
@@ -15,12 +15,16 @@ private:
 	const uint32_t frontRightChannel 	= 0;
 	const uint32_t backRightChannel 	= 3;
 
+	const uint32_t gyroChannel			= 0;
+
 	const uint32_t joystickChannel		= 0;
 
 	Talon* frontLeftWheel;
 	Talon* backLeftWheel;
 	Talon* frontRightWheel;
 	Talon* backRightWheel;
+
+	Gyro* roboGyro;
 
 	Joystick* stick;			// only joystick
 	OI* opIn;
@@ -44,14 +48,19 @@ public:
 		backLeftWheel = new Talon(backLeftChannel);
 		frontRightWheel = new Talon(frontRightChannel);
 		backRightWheel = new Talon(backRightChannel);
-		driveTrain = new MechanumDriveTrain(frontLeftWheel, backLeftWheel,frontRightWheel,backRightWheel,opIn);
+		roboGyro = new Gyro(gyroChannel);
+		driveTrain = new MechanumDriveTrain(frontLeftWheel, backLeftWheel,frontRightWheel,backRightWheel,opIn,roboGyro);
 
+		roboGyro->SetSensitivity(0.007);
+		roboGyro->SetDeadband(0.0014);
+		roboGyro->Reset();
 	}
 
 	~Robot() {
 		delete driveTrain;
 		delete opIn;
 		delete stick;
+		delete roboGyro;
 		delete frontLeftWheel;
 		delete backLeftWheel;
 		delete frontRightWheel;
@@ -67,14 +76,15 @@ public:
 		SmartDashboard::PutNumber("JoystickY",opIn->GetX());
 		SmartDashboard::PutNumber("JoystickX",opIn->GetY());
 		SmartDashboard::PutNumber("JoystickTwist",opIn->GetTwist());
+		SmartDashboard::PutNumber("GyroAngle",roboGyro->GetAngle());
+		SmartDashboard::PutNumber("GyroAngle",roboGyro->GetRate());
+		SmartDashboard::PutNumber("JoystickThrottle",opIn->GetThrottle());
 	}
 
 	void AutonomousInit() {
-
 	}
 
 	void AutonomousPeriodic() {
-		std::cout<<"In Autonomous"<<std::endl;
 	}
 
 };
